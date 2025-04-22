92c889ea2ec6f15c2759d14320fa0c75bb887f40
Sahil-214 <git@github.com:Sahil-214/ubuntu.git>
2025-04-22 13:29:55 +0530

    Updated main.tf for EC2 Terraform setup

--- infra/terraform/ec2/main.tf
+++ infra/terraform/ec2/main.tf
@@ -2,10 +2,63 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Create a new VPC
+resource "aws_vpc" "event_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "event-vpc"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "event_subnet" {
+  vpc_id                  = aws_vpc.event_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+
+  tags = {
+    Name = "event-subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "event_igw" {
+  vpc_id = aws_vpc.event_vpc.id
+
+  tags = {
+    Name = "event-igw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "event_rt" {
+  vpc_id = aws_vpc.event_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.event_igw.id
+  }
+
+  tags = {
+    Name = "event-rt"
+  }
+}
+
+# Associate the route table with subnet
+resource "aws_route_table_association" "event_assoc" {
+  subnet_id      = aws_subnet.event_subnet.id
+  route_table_id = aws_route_table.event_rt.id
+}
+
+# Create a security group in the new VPC
 resource "aws_security_group" "eventgroups" {
   name        = "eventgroups"
   description = "Allow SSH, HTTP and custom app access"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.event_vpc.id
 
   ingress {
     from_port   = 22
@@ -40,13 +93,11 @@ resource "aws_security_group" "eventgroups" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
+# Launch EC2 in the new VPC
 resource "aws_instance" "event_instance" {
-  ami                    = "ami-0ded8326293d3201b" # Ubuntu 22.04 LTS for ap-south-1
+  ami                    = "ami-0e35ddab05955cf57" # Ubuntu 22.04 LTS for ap-south-1
   instance_type          = "t2.medium"
+  subnet_id              = aws_subnet.event_subnet.id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.eventgroups.id]
 
@@ -56,4 +107,3 @@ resource "aws_instance" "event_instance" {
     Name = "event-web-ec2"
   }
 }
-
