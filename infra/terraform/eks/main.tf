a8e4fe983af71905e1737bf7e2668f2edc656823
Sahil-214 <git@github.com:Sahil-214/ubuntu.git>
2025-04-28 01:47:03 +0530

    add eks scipt

--- infra/terraform/eks/main.tf
+++ infra/terraform/eks/main.tf
@@ -2,13 +2,20 @@ provider "aws" {
   region = "us-east-1"  # Change to your preferred region
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+data "aws_vpc" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = ["event-vpc"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+  tags = {
+    Tier = "private"  # Adjust based on your subnet tagging
   }
 }
 
@@ -19,8 +26,8 @@ module "eks" {
   cluster_name    = "event-web-app"
   cluster_version = "1.27"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = data.aws_vpc.existing.id
+  subnet_ids = data.aws_subnets.private.ids
 
   eks_managed_node_groups = {
     default = {
@@ -30,19 +37,4 @@ module "eks" {
       desired_size  = 2
     }
   }
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = "event-web-app-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
 }
